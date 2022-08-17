9243d3ae76857f6797147c9a0278b28dd6e0d4f3
Sora33 <2097665736inori@gmail.com>
2022-08-17 20:36:24 +0800

    Site updated: 2022-08-17 20:36:19

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "4A86E8",
+    "tmpSize": "16",
+    "cityColor": "FF9900",
+    "citySize": "16",
+    "aqiColor": "4A86E8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "10",
+    "top": "10",
+    "key": "3c6f09584a7343d98bbf75f8b74ac88e"
+  }
+}
